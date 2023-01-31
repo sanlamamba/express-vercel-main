3823bd5aa9b72de1cb6c2b38160110f7b71d2bba
San Lamamba P <62671141+sanlamamba@users.noreply.github.com>
2023-01-31 16:15:56 +0000

    changing mongoose uri

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ const app = express();
 
 // mongoose
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://admin:7wK0VuTb0Qz0TPaZ@mams.pjugncd.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
